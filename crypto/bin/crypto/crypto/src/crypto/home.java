08cc7070bae5897f0020a5fff5e45d0eb642a0bf
outiti88 <51408603+outiti88@users.noreply.github.com>
2020-04-03 11:39:13 +0100

    Add files via upload

---
+++ crypto/bin/crypto/crypto/src/crypto/home.java
@@ -0,0 +1,46 @@
+package crypto;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
+public class home {
+
+	public static void main(String[] args) 
+	{
+	    final String secretKey = "r";
+	    
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Entrer votre texte: "); //le texte à Crypter
+	    
+	    String originalString = sc.nextLine();
+	   
+	    cesar.appCesar(originalString,4); // cesar
+	     
+	    
+	    AES.appAes(originalString, secretKey); //AES
+	    
+	    
+	    DES.appDes(originalString, secretKey); //DES
+	    
+	    Blowfish.appBlowfish(originalString, secretKey); //Blowfish
+	    
+	    System.out.println("*************************");
+	    System.out.println("algorithme RSA");
+	    RSA rsa = new RSA(1024);
+	    System.out.println("Plaintext: " + originalString);
+	    BigInteger plaintext = new BigInteger(originalString.getBytes());
+
+	    BigInteger ciphertext = rsa.encrypt(plaintext);
+	    System.out.println("Ciphertext: " + ciphertext);
+	    plaintext = rsa.decrypt(ciphertext);
+
+	    String text2 = new String(plaintext.toByteArray());
+	    System.out.println("Plaintext: " + text2);
+	    
+	    
+	    
+	    
+	    
+	}
+}
